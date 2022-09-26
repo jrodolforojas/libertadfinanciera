c8e4efcb8cd2009440163c94ed9a24f44c18f756
Jose Rodolfo Rojas <jrodolforojas1@gmail.com>
2022-09-26 14:37:46 -0600

    Show csv data formatted

--- src/App.js
+++ src/App.js
@@ -4,10 +4,9 @@ import Papa from 'papaparse'
 const ALLOWED_EXTENSIONS = ['csv']
 
 function App () {
-  const [data, setData] = useState()
-  console.log('🚀 ~ file: App.js ~ line 8 ~ App ~ data', data)
+  const [transactions, setTransactions] = useState([])
+  console.log('🚀 ~ file: App.js ~ line 8 ~ App ~ transactions', transactions)
   const [error, setError] = useState()
-  console.log('🚀 ~ file: App.js ~ line 10 ~ App ~ error', error)
   const [file, setFile] = useState()
 
   const handleFileChange = event => {
@@ -23,6 +22,18 @@ function App () {
     }
   }
 
+  const convertCSVDataToObject = (transaction) => {
+    return {
+      id: transaction[1],
+      date: transaction[0],
+      type: transaction[2],
+      description: transaction[3],
+      debit: transaction[4],
+      credit: transaction[5],
+      balance: transaction[6]
+    }
+  }
+
   const handleParse = () => {
     if (!file) {
       return setError('Enter a valid file')
@@ -34,10 +45,14 @@ function App () {
     // loads, we parse it and set the data.
     reader.onload = async ({ target }) => {
       const csv = Papa.parse(target.result)
-      const parsedData = csv.data
-      console.log('🚀 ~ file: App.js ~ line 38 ~ reader.onload= ~ parsedData', parsedData)
-      const columns = Object.keys(parsedData[4])
-      setData(columns)
+      const csvData = csv.data
+
+      const parsedData = []
+      csvData.slice(1).forEach(transaction => {
+        const objectTransaction = convertCSVDataToObject(transaction)
+        parsedData.push(objectTransaction)
+      })
+      setTransactions(parsedData)
     }
     reader.readAsText(file)
   }
@@ -50,7 +65,31 @@ function App () {
         </h1>
         <input type="file" name="file" onChange={handleFileChange}/>
         <button className="bg-blue-600 p-3 mt-3 text-white rounded-md" onClick={handleParse}>Scan</button>
+        <table className='w-1/2'>
+          <tr className='text-blue-800'>
+            <th>Date</th>
+            <th>Type</th>
+            <th>Description</th>
+            <th>Debit</th>
+            <th>Credit</th>
+            <th>Balance</th>
+          </tr>
+          {transactions.map((transaction, index) => {
+            return (
+              <tr key={index} className='mt-2 text-center'>
+                <td>{transaction.date}</td>
+                <td>{transaction.type}</td>
+                <td>{transaction.description}</td>
+                <td>{transaction.debit}</td>
+                <td>{transaction.credit}</td>
+                <td>{transaction.balance}</td>
+              </tr>
+            )
+          }
+          )}
+        </table>
       </div>
+      {error !== undefined && <p>{error}</p>}
     </div>
   )
 }
